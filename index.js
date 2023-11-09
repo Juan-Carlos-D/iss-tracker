93e72762ab8dadde46a6652fcebf0e68907a7410
Juan Carlos Dominguez <jcarlosdominguez38@gmail.com>
2023-11-08 20:00:12 -0800

    Tracking path

--- index.js
+++ index.js
@@ -30,8 +30,8 @@ map.on('click', onMapClick);
 const issApiUrl = 'http://api.open-notify.org/iss-now.json';
 
 const issIcon = L.icon({
-    iconUrl: 'assets/iss.png',
-    iconSize: [50, 32],
+    iconUrl: 'assets/space-station-3.png',
+    iconSize: [50, 50],
     iconAnchor: [25, 16]
 })
 
@@ -68,3 +68,40 @@ setInterval(getISSLocation, 2000);
 
 //Initial call to get ISS location
 getISSLocation();
+
+// Create an array to store the ISS path coordinates
+var issPathCoordinates = [];
+
+// Function to fetch ISS location data and update the map and path
+function updateISSLocation() {
+    fetch(issApiUrl)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            const latitude = data.iss_position.latitude;
+            const longitude = data.iss_position.longitude;
+
+            // Use the lat and long to update the map or marker
+            updateMap(latitude, longitude);
+
+            // Add the new coordinates to the path
+            issPathCoordinates.push([latitude, longitude]);
+            
+            // Update the path with the new coordinates
+            issPath.setLatLngs(issPathCoordinates);
+
+            // Update the HTML elements with the new data
+            document.getElementById('iss-lat').textContent = latitude;
+            document.getElementById('iss-lon').textContent = longitude;
+        })
+        .catch(error => console.error('Error fetching ISS location data:', error));
+}
+
+// Create a polyline (path) for the ISS
+var issPath = L.polyline(issPathCoordinates, { color: 'red' }).addTo(map);
+
+// Fetch ISS location data and update at regular intervals (e.g., every 2 seconds)
+setInterval(updateISSLocation, 2000);
+
+// Initial call to get ISS location
+updateISSLocation();
